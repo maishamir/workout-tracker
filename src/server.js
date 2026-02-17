4d14ef6c24bb48d3f78f11e6d9d064c3559f96e6
Maisha Mir <maisha.mir2014@gmail.com>
2026-02-17 15:03:27 -0800

    specify cors origin

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ import sessionsRouter from "./routes/sessions.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 app.use(express.json());
 
 app.use("/exercises", exerciseRouter);
