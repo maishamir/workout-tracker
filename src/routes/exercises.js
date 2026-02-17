c600847c93fb78001c9a58a87b9ff71a39968e7b
Maisha Mir <maisha.mir2014@gmail.com>
2026-02-16 18:03:46 -0800

    post new exercise to db

--- src/routes/exercises.js
+++ src/routes/exercises.js
@@ -17,4 +17,38 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.post("/", async (req, res) => {
+    try {
+        const { name, primaryMuscleGroup, equipment, type, isCustom } = req.body;
+
+        // validation
+        if (!name || !primaryMuscleGroup || !equipment || !type) {
+            return res.status(400).json({
+                error: "name, primaryMuscleGroup, equipment, and type are required",
+            });
+        }
+
+        const newExercise = await prisma.exercise.create({
+            data: {
+                name, primaryMuscleGroup, equipment, type, isCustom: isCustom ?? true,
+            },
+        });
+
+        res.status(201).json(newExercise);
+    }
+
+    catch (error) {
+        console.error(error);
+
+        //throw error if error code indicates duplicate entry
+        if (error.code === "P2002") {
+            return res.status(409).json({
+                error: "Exercise with this name already exists",
+            });
+        }
+
+        res.status(500).json({ error: "Failed to create exercise" });
+    }
+})
+
 export default router;
