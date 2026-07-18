7309ad3a95cd193bc525885910955c05efc0c108
Maisha Mir <maisha.mir2014@gmail.com>
2026-07-17 22:33:51 -0700

    create routes to create user and get their sessions and routines

--- src/routes/users.js
+++ src/routes/users.js
@@ -45,10 +45,71 @@ router.post('/', async (req, res) => {
 
 // routines.js
 // TODO: GET /routines - get ALL routines for the signed-in user
+router.get('/routines', async (req, res) => {
+    console.log(req.body)
+    try {
+        const { userId } = req.query;
+        if (!userId) {
+            return res.status(400).json({ error: "userId is required" });
+        }
+
+        const userRoutines = await prisma.routine.findMany({
+            orderBy: { name: "asc" },
+            where: {
+                userId: userId
+            },
+            include: {
+                routineExercises: {
+                    include: {
+                        exercise: true,
+                        routineSets: true
+                    }
+                }
+            }
+        })
+        res.json(userRoutines)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to fetch user's routines" })
+    }
+
+
+})
 // TODO: PUT /routines/:id - update routine (already exists?)
 // TODO: DELETE /routines/:id - delete routine (already exists?)
 
 // workoutSessions.js (or sessions.js)
 // TODO: GET /sessions - get all sessions for the signed-in user (filtered by userId)
+router.get("/sessions", async (req, res) => {
+    console.log(req.body);
+
+    try {
+        const { userId } = req.query;
+        if (!userId) {
+            return res.status(400).json({ error: "userId is required" })
+        }
+
+        const userWorkoutSessions = await prisma.workoutSession.findMany({
+            orderBy: { date: "asc" },
+            where: { userId: userId },
+            include: {
+                sessionExercises: {
+                    orderBy: [{ sessionLabel: "asc" }, { orderIndex: "asc" }],
+                    include: {
+                        exercise: true,
+                        sessionSets: {
+                            orderBy: { orderIndex: "asc" }
+                        }
+                    }
+                }
+            }
+        })
+        res.json(userWorkoutSessions);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to fetch user's workout sessions" });
+    }
+
+})
 // TODO: PUT /sessions/:id - mark session complete
 // TODO: DELETE /sessions/:id - remove session
